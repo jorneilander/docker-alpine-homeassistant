4a53d75c58af63494dd09a0748685dd495016c2a
Jorn Eilander <jorn.eilander@azorion.com>
2021-02-08 21:12:23 +0100

    Bump version and add mandatory components

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # set -x
 
-HASS_VERSION=2021.2.0
+HASS_VERSION=2021.2.1
 ALPINE_VERSION=3.12
 IMAGE_NAME=failfr8er/home-assistant
 
@@ -11,11 +11,9 @@ docker buildx build \
   --cache-to=type=local,dest=/tmp/.buildx \
   --tag ${IMAGE_NAME}:${HASS_VERSION} \
   --tag ${IMAGE_NAME}:latest \
-  --push \
+  --"${1:-load}" \
   --build-arg ALPINE_VERSION=${ALPINE_VERSION} \
   --build-arg HASS_VERSION=${HASS_VERSION} \
-  --platform=linux/amd64,linux/arm64,linux/arm/v7 \
+  --platform=linux/amd64 \
   --progress plain \
   .
-
-# ,linux/arm64,linux/arm/v7
